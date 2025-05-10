d6c2d4d7e12690b1d021f0642bb02e8eec7a825a
Roberto <robertomdev@gmail.com>
2025-05-10 12:24:15 +0200

    * Added map example with some markers.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ These libraries are "unrelated"in the sense that are develop by different people
 
 There is a third dependency that we will be using for Typescript support: "@types/leaflet"
 
-## Docs
+# Docs
 
 - [Leaflet tutorials](https://leafletjs.com/examples.html)
 - [Leaflet docs](https://leafletjs.com/reference.html)
@@ -19,6 +19,14 @@ There is a third dependency that we will be using for Typescript support: "@type
 
 - [React Leaflet installation](https://react-leaflet.js.org/docs/start-introduction/)
 - [React Leaflet examples](https://react-leaflet.js.org/docs/example-popup-marker/)
+
+# Notes
+
+A couple of notes I want to mention regarding these libraries:
+- The "hot-reloading" features seems to not work pretty well with it, so sometimes
+  you should do a manual reload of the page when making changes related to the maps.
+- When you make the setup of the project don't forget to add the .css import in the
+  component that will integrate a map: `import 'leaflet/dist/leaflet.css';`
 
 # How to...
 
--- src/examples/MapFeatures.tsx
+++ src/examples/MapFeatures.tsx
@@ -1,4 +1,36 @@
+import 'leaflet/dist/leaflet.css';
+import {MapContainer, Marker, Popup, TileLayer, Tooltip} from "react-leaflet";
+
+enum TileOption { TILE_A, TILE_B, TILE_C }
+
 export default function MapFeatures() {
+    const tileOption: TileOption = TileOption.TILE_C
+
+    interface LocationsCoordinates {
+        beijing: [number, number];
+        xian: [number, number];
+        shanghai: [number, number];
+        kunming: [number, number]
+    }
+
+    const coords: LocationsCoordinates = {
+        beijing: [39.9042, 116.4074],
+        xian: [34.26, 108.9398],
+        shanghai: [31.2304, 121.4737],
+        kunming: [25.038, 102.718]
+    }
+
+    function getGeographicCenter(coords: [number, number][]): [number, number] {
+        const total = coords.reduce(
+            (acc, [lat, lon]) => [acc[0] + lat, acc[1] + lon] as [number, number],
+            [0, 0]
+        );
+
+        const count = coords.length || 1;
+
+        return [total[0] / count, total[1] / count];
+    }
+
     return <div className={`
         w-full
         h-screen
@@ -12,7 +44,61 @@ export default function MapFeatures() {
             border-1 border-black/10
             bg-black/5
         `}>
-
+            <MapContainer
+                /*
+                    The 'getGeographicCenter' method is simple a support
+                    method whose goal is to always set as the center of
+                    the map the center point among all the marker points
+                 */
+                center={getGeographicCenter(Object.values(coords))}
+                zoom={4}
+                scrollWheelZoom={true}
+                touchZoom={true}
+                zoomSnap={0.7}
+                attributionControl={false}
+                className={`w-full h-full`}
+            >
+                {
+                    (() => {
+                        switch (tileOption) {
+                            default:
+                            // @ts-ignore
+                            case TileOption.TILE_A:
+                                return <TileLayer
+                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                            // @ts-ignore
+                            case TileOption.TILE_B:
+                                return <TileLayer
+                                    attribution='&copy; <a href="https://carto.com/">CARTO</a> | &copy; OpenStreetMap contributors'
+                                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                                />
+                            case TileOption.TILE_C:
+                                return <TileLayer
+                                    attribution='&copy; <a href="https://carto.com/">CARTO</a> | &copy; OpenStreetMap contributors'
+                                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                                />
+                        }
+                    })()
+                }
+                <Marker position={coords.beijing}>
+                    <Popup>Beijing</Popup>
+                    <Tooltip>Beijing</Tooltip>
+                </Marker>
+                <Marker position={coords.xian}>
+                    <Popup>Xi'an</Popup>
+                    <Tooltip>Xi'an</Tooltip>
+                </Marker>
+                <Marker position={coords.shanghai}>
+                    <Popup>Shanghai</Popup>
+                    <Tooltip>Shanghai</Tooltip>
+                </Marker>
+                <Marker position={coords.kunming}>
+                    <Popup>Kunming</Popup>
+                    <Tooltip>Kunming</Tooltip>
+                </Marker>
+            </MapContainer>
         </div>
     </div>
 }
